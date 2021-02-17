5046533af62fbc6edf6b84eab5fd3f6e428b44f4
PuBHARGAVI <saibhargavi.puvvada@gmail.com>
2021-02-17 19:43:02 +0530

    1499215

---
+++ app.js
@@ -0,0 +1,26 @@
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            isLogged:true,
+            details:[
+                {id:1,name:"Bhargavi",age:20},
+                {id:2,name:"Jahnavi",age:19},
+                {id:3,name:"Vyshnavi",age:22}
+            ]
+        }
+    }
+    render(){
+        return(
+            <div>
+            {(this.state.isLogged) && this.state.details.map(detail=>(
+                <div key={detail.id} className="person">
+                    <h1>{detail.name}</h1>
+                    <p>{detail.age}</p>
+                </div>
+            ))}
+            </div>
+        );
+    }
+}
+ReactDOM.render(<App/>,document.getElementById("root"));
